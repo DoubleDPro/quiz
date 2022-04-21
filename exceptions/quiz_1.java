6ebcc652f2fc77e433b038839fddff57a299b19a
Дмитрий <37687109+DoubleDPro@users.noreply.github.com>
2022-04-21 10:27:43 +0300

    Update quiz_1.java in exceptions

--- exceptions/quiz_1.java
+++ exceptions/quiz_1.java
@@ -1,1 +1,19 @@
+public class Quiz {
 
+    public static void main(String[] args) {
+        try {
+            method();
+        } catch (IllegalArgumentException e) {
+            System.out.println("IllegalArgumentException");
+        } catch (RuntimeException e) {
+            System.out.println("RuntimeException");
+        } catch (Exception e) {
+            System.out.println("Exception");
+        }
+    }
+    
+    private static void method() {
+        throw new RuntimeException();
+    }
+    
+}
