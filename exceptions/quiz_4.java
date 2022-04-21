5482d8baddce520c23193e7c4eef533016883373
Дмитрий <37687109+DoubleDPro@users.noreply.github.com>
2022-04-21 10:44:28 +0300

    Create quiz_4.java in exceptions

---
+++ exceptions/quiz_4.java
@@ -0,0 +1,17 @@
+public class Quiz {
+
+    public static void main(String[] args) {
+         System.out.println(method());
+    }
+    
+    private static int method() {
+        try {
+            int i = 5;
+            return i;
+        } catch (Exception e) {
+            System.out.println("Exception");
+        } finally {
+            return 6;
+        }
+    }
+}
