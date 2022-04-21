6590484d8fda10b4d976db952f665162c5b25eef
Дмитрий <37687109+DoubleDPro@users.noreply.github.com>
2022-04-21 08:57:14 +0300

    Create quiz_2.java in basics

---
+++ basic/quiz_2.java
@@ -0,0 +1,12 @@
+public class Quiz {
+
+    public static void main(String[] args) {
+      int i = 55;
+      
+      if (i > 50)
+        System.out.println("Больше 50");
+      else
+        System.out.println("Меньше 50");
+        System.out.println("Готово");
+    }
+}
