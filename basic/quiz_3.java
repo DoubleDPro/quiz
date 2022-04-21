e8dfc1e0d2fcb58cf9274ca4430abeeb76bac234
Дмитрий <37687109+DoubleDPro@users.noreply.github.com>
2022-04-21 09:02:42 +0300

    Create quiz_3.java in basics

---
+++ basic/quiz_3.java
@@ -0,0 +1,11 @@
+public class Quiz {
+
+    public static void main(String[] args) {
+      for (byte b = 1; b < 130; b++) {
+          if (b  == 128) {
+              break;
+          }
+          System.out.println(b);
+      }
+    }
+}
