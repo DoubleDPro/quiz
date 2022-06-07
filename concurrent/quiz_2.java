f6b06dc2919dcf0695e3a2c5aba8bdb2e3f76ab9
Дмитрий <37687109+DoubleDPro@users.noreply.github.com>
2022-06-07 14:01:51 +0300

    Create quiz_2.java in concurrent

---
+++ concurrent/quiz_2.java
@@ -0,0 +1,32 @@
+public class Main {
+	public static void main(String[] args) throws InterruptedException {
+		Thread thread = new Thread(new Runner());
+		thread.start();
+		thread.join();
+		for (int i = 0; i < 10; i++) {
+		    try {
+		        Thread.sleep(1000);
+		        System.out.println("B");
+		    } catch (InterruptedException e) {
+		        // NOP
+		    }
+		    
+		}
+	}
+}
+
+class Runner implements Runnable {
+    
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(1000);
+                System.out.println("A");
+               
+            } catch (InterruptedException e) {
+                // NOP
+            }
+        }
+    }
+}
