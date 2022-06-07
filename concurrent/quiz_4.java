5a6dcaa15779e1d7222092ba226da059c44a32ff
Дмитрий <37687109+DoubleDPro@users.noreply.github.com>
2022-06-07 15:56:29 +0300

    Update quiz_4.java in concurrent

--- concurrent/quiz_4.java
+++ concurrent/quiz_4.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Quiz {
 	public static void main(String[] args) throws InterruptedException {
 		CommonResource commonResource = new CommonResource();
 		for (int i = 0; i < 10; i++) {
